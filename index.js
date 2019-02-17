6145e02e96bea284e0587c5a9080eae51764581f
Bolkonyashka <33488476+Bolkonyashka@users.noreply.github.com>
2019-02-17 21:02:05 +0500

    refact + comments

--- index.js
+++ index.js
@@ -9,39 +9,54 @@ function app () {
     readLine(processCommand);
 }
 
+/*
+    Возвращает массив, в котором каждому .js файлу текущей директории соответствует 
+    объект с полями path (путь к файлу) и data (текст файла)
+*/
 function getFiles () {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
 
     return filePaths.map((path) => { return { path: path, data: readFile(path) } });
 }
 
-function findComments (fileObj) {
+/*
+    Ищет todo-комментарии в тексте данного файла, структурирует их с помощью structComment(),
+    наполняет аккумулирующий массив для reduce()
+*/
+function findComments (acc, fileObj) {
     const pattern = /\/\/\s*TODO\s*[\s:]{1}\s*.*/ig;
-    let match = fileObj.data.match(pattern);
-    let result = [];
+    const match = fileObj.data.match(pattern);
+    console.log(match);
     if (match) {
-        result = match.map(comment => comment.replace(/\/\/\s*TODO\s*[\s:]{1}\s*/ig, ''));
-    }
-    fileObj.data = result; // TODO не переиспользовать data, а ввести еще одно свойство - comments?
+        acc = acc.concat(match.map(comment => structComment(comment, fileObj.path)));
+    } // TODO не переиспользовать data, а ввести еще одно свойство - comments?
 
-    return fileObj;
+    return acc;
 }
 
+/*
+    Возвращает количество восклицательных знаков в переданном тексте
+*/
 function getImportanceValue (text) {
     const result = text.match(/!/g);
-    if (!result) {
-        return 0;
-    }
 
-    return result.length;
+    return result? result.length : 0;
 }
 
+/*
+    Проверяет валидность даты для форматов yyyy-mm-dd и dd-mm-yyyy,
+    пробелы в переданной строке игнорируются
+*/
 function checkDate (dateString) { // TODO :roma; 15 - 02 - 2019; стоит ли оставлять ограничение до 3 тысячелетия? :)
     const pattern = /^[1-9]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$|^(0[1-9]|[12][0-9]|3[01])-(0[1-9]|1[0-2])-[1-9]\d{3}$/;
 
     return pattern.test(dateString.replace(/\s+/g, ''));
 }
 
+/*
+    Удаляет пробелы из строки с датой,
+    если дата в формате dd-mm-yyyy, перводит в формат yyyy-mm-dd
+*/
 function normalizeDate (dateString) {
     let result = dateString.replace(/\s+/g, '');
     const pattern = /(\d\d)-(\d\d)-(\d{4})/;
@@ -53,36 +68,49 @@ function normalizeDate (dateString) {
     return result;
 }
 
-function structComments (acc, fileObj) {
-    const fileName = fileObj.path.replace(/^.*[\\\/]/, '');
-    const pattern = /((.*);(.*);){0,1}(.*)/i; // /((.*){0,1};\s*(\d{4}\s*-\s*\d\d\s*-\s*\d\d){0,1}\s*;){0,1}(.*)/i
+/*
+    Возвращает объект с данными о комментарии
+*/
+function structComment (comment, path) {
+    const fileName = path.replace(/^.*[\\\/]/, '');
+    const pattern = /\/\/\s*TODO\s*[\s:]{1}\s*((.*);(.*);){0,1}(.*)/i;
+    const match = comment.match(pattern);
 
-    for (let comment of fileObj.data) { 
-        const match = comment.match(pattern);
-        acc.push({
-            importance: getImportanceValue(match[4]),
-            user: match[2] && match[2].trim() || '',
-            date: match[3] && checkDate(match[3]) ? normalizeDate(match[3]) : '',
-            text: match[4].trim(),
-            file: fileName });
-    }
-
-    return acc;
+    return {
+        importance: getImportanceValue(match[4]),
+        user: match[2] ? match[2].trim() : '',
+        date: match[3] && checkDate(match[3]) ? normalizeDate(match[3]) : '',
+        text: match[4].trim(),
+        file: fileName 
+    };
 }
 
+/*
+    Возвращет массив, состоящий из всех todo коментариев в .js файлах 
+    текущей директори, структурированных в объекты
+*/
 function getComments () {
-    const filesData = getFiles();
-    return filesData.map(findComments).reduce(structComments, []);
+    return getFiles().reduce(findComments, []);
 }
 
+/*
+    Выводит таблицу со всеми комментариями
+*/
 function printAll () {
     printTable(getComments());
 }
 
+/*
+    Выводит таблицу с комментариями, в которых есть восклицательный знак
+*/
 function printImportant () {
     printTable(getComments().filter(comm => comm.importance > 0));
 }
 
+/*
+    Выводит таблицу с комментариями, созданными пользователем, никнейм 
+    которого начинается на указанный пользователем префикс или совпадает с ним
+*/
 function printByUser(username) { // tODo : romochka;;обрабатывать пустой и неопределенный юзернейм
     if (username) {
         printTable(getComments().filter(comm => comm.user.toLowerCase().indexOf(username.toLowerCase()) === 0));
@@ -177,4 +205,6 @@ function processCommand (command) {
 // TODO you can do it!
 //               TODO           saskeUzum@k1-kokok sasai   ;   2019-01-01  ;   CHTOTOTTUTNETAK
 //TODO                                                      
-//todo roma@4232@@44954""""";04-10-1996;  benedick cumberskotch
+//todo roma@4232@@44954""""";04-10-1996;  benedick cumberskotch!!!!!!!!!!
+//TODO roma;17 - 02-2019;
+//todo:;;
