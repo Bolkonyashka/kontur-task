7e9733b48cf4ca6c0426490f9feec90fda9938c9
Bolkonyashka <33488476+Bolkonyashka@users.noreply.github.com>
2019-02-22 20:53:16 +0500

    hello, eslint!

--- index.js
+++ index.js
@@ -2,13 +2,6 @@ const { getAllFilePathsWithExtension, readFile } = require('./fileSystem');
 const { readLine } = require('./console');
 const { printTable } = require('./tablePrinter');
 
-app();
-
-function app () {
-    console.log('Please, write your command!');
-    readLine(processCommand);
-}
-
 /**
  * @typedef {Object} Comment Структурированный комментарий
  * @property {Number} id - id комментария
@@ -29,10 +22,10 @@ function app () {
  * Возвращает массив объектов с данными о каждом .js файле текущей директории
  * @returns {FileObject[]} Результирующий массив
  */
-function getFiles () {
+function getFiles() {
     const filePaths = getAllFilePathsWithExtension(process.cwd(), 'js');
 
-    return filePaths.map((path) => { return { path: path, data: readFile(path) }; });
+    return filePaths.map(path => ({ path, data: readFile(path) }));
 }
 
 /**
@@ -40,7 +33,7 @@ function getFiles () {
  * @param {String} text Текст для поиска
  * @returns {Number} Количество восклицательных знаков
  */
-function getExclamationCount (text) {
+function getExclamationCount(text) {
     const result = text.match(/!/g);
 
     return result ? result.length : 0;
@@ -52,7 +45,7 @@ function getExclamationCount (text) {
  * @param {String} dateString Дата в формате yyyy[-mm[-dd]]
  * @returns {String} Дата в формате yyyy-mm-dd или пустая строка
  */
-function extendDate (dateString) {
+function extendDate(dateString) {
     const pattern = /^(\d{4})(-(\d\d)){0,1}(-(\d\d)){0,1}$/;
     const match = dateString.match(pattern);
     return match ? `${match[1]}-${match[3] || '01'}-${match[5] || '01'}` : '';
@@ -63,7 +56,7 @@ function extendDate (dateString) {
  * @param {String} dateString Дата в формате yyyy-mm-dd
  * @returns {Boolean} Результат проверки
  */
-function checkDate (dateString) {
+function checkDate(dateString) {
     const pattern = /^[1-9]\d{3}(-(0[1-9]|1[0-2])(-(0[1-9]|[12][0-9]|3[01])){0,1}){0,1}$/;
 
     return pattern.test(dateString.replace(/ +/g, ''));
@@ -74,12 +67,12 @@ function checkDate (dateString) {
  * @param {String} dateString Дата
  * @returns {String} Строковое представление даты в каноничном формате
  */
-function normalizeDate (dateString) {
+function normalizeDate(dateString) {
     let result = dateString.replace(/ +/g, '');
     const pattern = /^((\d\d)-){0,1}(\d\d)-(\d{4})$/;
     const match = result.match(pattern);
     if (match) {
-        result = match[4] + '-' + match[3] + (match[1] ? '-' + match[2] : '');
+        result = `${match[4]}-${match[3]}${match[1] ? `-${match[2]}` : ''}`;
     }
 
     return result;
@@ -92,9 +85,9 @@ function normalizeDate (dateString) {
  * @returns {String} Дата в формате yyyy[-mm[-dd]] или пустая строка, если дата некорректна
  */
 function handleDateString(dateString) {
-    dateString = normalizeDate(dateString);
+    const normalDate = normalizeDate(dateString);
 
-    return checkDate(dateString) ? dateString : '';
+    return checkDate(normalDate) ? normalDate : '';
 }
 
 /**
@@ -104,8 +97,8 @@ function handleDateString(dateString) {
  * @param {Number} counter Номер комментария в порядке просмотра
  * @returns {Comment} Объект c информацией о комментарии
  */
-function structComment (comment, path, counter) {
-    const fileName = path.replace(/^.*[\\\/]/, '');
+function structComment(comment, path, counter) {
+    const fileName = path.replace(/^.*[\\/]/, '');
     const pattern = /\/\/ *TODO *[ :]{1} *(([^;]*);([^;]*);){0,1}(.*)/i;
     const match = comment.match(pattern);
 
@@ -115,7 +108,7 @@ function structComment (comment, path, counter) {
         user: match[2] ? match[2].trim() : '',
         date: match[3] ? handleDateString(match[3]) : '',
         text: match[4].trim(),
-        file: fileName 
+        file: fileName,
     };
 }
 
@@ -130,7 +123,7 @@ function findComments(acc, fileObj) {
     const pattern = /\/\/ *TODO *[ :]{1} *.*$/igm;
     const match = fileObj.data.match(pattern);
     if (match) {
-        acc = acc.concat(match.map(comment => structComment(comment, fileObj.path, counter++)));
+        return acc.concat(match.map(comment => structComment(comment, fileObj.path, counter++)));
     }
 
     return acc;
@@ -142,21 +135,21 @@ function findComments(acc, fileObj) {
  * комментариев без текста, даты и имени пользователя
  * @returns {Comment[]} Массив комментариев, структурированных в объекты
  */
-function getComments () {
+function getComments() {
     return getFiles().reduce(findComments, []).filter(comm => comm.date || comm.user || comm.text);
 }
 
 /**
  * Выводит таблицу со всеми комментариями
  */
-function printAll () {
+function printAll() {
     printTable(getComments());
 }
 
 /**
  * Выводит таблицу с комментариями, в которых есть хоть один восклицательный знак
  */
-function printImportant () {
+function printImportant() {
     printTable(getComments().filter(comm => comm.importance > 0));
 }
 
@@ -167,7 +160,8 @@ function printImportant () {
  */
 function printByUser(username) {
     if (username) {
-        printTable(getComments().filter(comm => comm.user.toLowerCase().indexOf(username.toLowerCase()) === 0));
+        printTable(getComments()
+            .filter(comm => comm.user.toLowerCase().indexOf(username.toLowerCase()) === 0));
     } else {
         console.log('Enter username');
     }
@@ -181,7 +175,7 @@ function printByUser(username) {
  * @returns {Number} Отрицательное число, если первая строка меньше второй,
  * положительное, если больше, ноль, если строки эквивалентны.
  */
-function compareStrings (a, b) {
+function compareStr(a, b) {
     return !a && b ? 1 : a && !b ? -1 : a.localeCompare(b);
 }
 
@@ -192,18 +186,20 @@ function compareStrings (a, b) {
 function printSorted(sortType) {
     const commandTip = 'Enter sort type: importance | user | date';
     switch ((sortType || '').toLowerCase()) {
-        case 'importance':
-            printTable(getComments().sort((a, b) => b.importance - a.importance || a.id - b.id));
-            break;
-        case 'user':
-            printTable(getComments().sort((a, b) => compareStrings(a.user.toLowerCase(), b.user.toLowerCase()) || a.id - b.id));
-            break;
-        case 'date':
-            printTable(getComments().sort((a, b) => -extendDate(a.date).localeCompare(extendDate(b.date)) || a.id - b.id));
-            break;
-        default:
-            console.log(commandTip);
-            break;
+    case 'importance':
+        printTable(getComments().sort((a, b) => b.importance - a.importance || a.id - b.id));
+        break;
+    case 'user':
+        printTable(getComments()
+            .sort((a, b) => compareStr(a.user.toLowerCase(), b.user.toLowerCase()) || a.id - b.id));
+        break;
+    case 'date':
+        printTable(getComments()
+            .sort((a, b) => -extendDate(a.date).localeCompare(extendDate(b.date)) || a.id - b.id));
+        break;
+    default:
+        console.log(commandTip);
+        break;
     }
 }
 
@@ -211,14 +207,13 @@ function printSorted(sortType) {
  * Выводит таблицу с комментариями, созданными после указанной даты, включая её
  * @param {String} date Строковое представление даты
  */
-function printByDate (date) {
+function printByDate(date) {
     const commandTip = 'Enter the correct date in one of the formats: yyyy | yyyy-mm | yyyy-mm-dd | mm-yyyy | dd-mm-yyyy';
     const fullDate = extendDate(handleDateString(date || ''));
-    if(fullDate) 
-    {
+    if (fullDate) {
         printTable(getComments()
             .filter(comm => extendDate(comm.date).localeCompare(fullDate) >= 0)
-            .sort((a, b) => extendDate(a.date).localeCompare(extendDate(b.date)) || a.id - b.id));    
+            .sort((a, b) => extendDate(a.date).localeCompare(extendDate(b.date)) || a.id - b.id));
     } else {
         console.log(commandTip);
     }
@@ -228,30 +223,37 @@ function printByDate (date) {
  * Анализирует и выполняет введенную пользователем команду
  * @param {String} command Комманда для выполнения
  */
-function processCommand (command) {
+function processCommand(command) {
     const splitted = command.split(/ +/);
     const [commandType, commandArg] = [splitted[0], splitted.slice(1).join(' ')];
     switch (commandType.toLowerCase()) {
-        case "show":
-            printAll();
-            break;
-        case "important":
-            printImportant();
-            break;
-        case "user":
-            printByUser(commandArg);
-            break;
-        case "sort":
-            printSorted(commandArg);
-            break;
-        case "date":
-            printByDate(commandArg);
-            break;
-        case "exit":
-            process.exit(0);
-            break;
-        default:
-            console.log("wrong command");
-            break;
+    case 'show':
+        printAll();
+        break;
+    case 'important':
+        printImportant();
+        break;
+    case 'user':
+        printByUser(commandArg);
+        break;
+    case 'sort':
+        printSorted(commandArg);
+        break;
+    case 'date':
+        printByDate(commandArg);
+        break;
+    case 'exit':
+        process.exit(0);
+        break;
+    default:
+        console.log('wrong command');
+        break;
     }
 }
+
+function app() {
+    console.log('Please, write your command!');
+    readLine(processCommand);
+}
+
+app();
--- tablePrinter.js
+++ tablePrinter.js
@@ -29,39 +29,43 @@ function getLimitedMax(first, second, limit) {
 }
 
 /**
- * Создает объект конфигурации с данными о длинах колонок. 
+ * Создает объект конфигурации с данными о длинах колонок.
  * Ширина колонок подбирается по самому длинному значению в колонке, но не больше максимальной.
  * @param {Comment[]} data Массив комментариев
  * @returns {Config} Объект конфигурации
  */
-function getLengthConfig (data) {
+function getLengthConfig(data) {
     const config = {
         userLength: 4,
         dateLength: 4,
         commentLength: 7,
         fileNameLength: 8,
-        fullLength: 0
+        fullLength: 0,
     };
-    for (let comment of data) {
+    for (const comment of data) {
         config.userLength = getLimitedMax(config.userLength, comment.user.length, 10);
         config.dateLength = getLimitedMax(config.dateLength, comment.date.length, 10);
         config.commentLength = getLimitedMax(config.commentLength, comment.text.length, 50);
         config.fileNameLength = getLimitedMax(config.fileNameLength, comment.file.length, 15);
     }
-    config.fullLength = 25 + config.userLength + config.dateLength + config.commentLength + config.fileNameLength;
+    config.fullLength = 25
+        + config.userLength
+        + config.dateLength
+        + config.commentLength
+        + config.fileNameLength;
 
     return config;
 }
 
 /**
- * Дополняет текст для колонки пробелами, если его длина меньше размера колонки, в противном случае 
+ * Дополняет текст для колонки пробелами, если его длина меньше размера колонки, в противном случае
  * обрезает текст
  * @param {String} text Текст для колонки
  * @param {Number} maxLen Размер колонки
  * @returns {String} Обработанный текст
  */
 function handleCellText(text, maxLen) {
-    return text.length <= maxLen ? text + ' '.repeat(maxLen - text.length) : text.substr(0, maxLen - 3) + '...';
+    return text.length <= maxLen ? text + ' '.repeat(maxLen - text.length) : `${text.substr(0, maxLen - 3)}...`;
 }
 
 /**
@@ -70,7 +74,7 @@ function handleCellText(text, maxLen) {
  * @param {Config} config Объект конфигурации
  * @returns {String} Строка таблицы
  */
-function formTableRow (comment, config) {
+function formTableRow(comment, config) {
     const isImportant = comment.importance > 0 ? '!' : ' ';
     const username = handleCellText(comment.user, config.userLength);
     const date = handleCellText(comment.date, config.dateLength);
@@ -86,22 +90,22 @@ function formTableRow (comment, config) {
  */
 function printTable(data) {
     const config = getLengthConfig(data);
-    let table = `  !  |  ${handleCellText('user', config.userLength)}  ` + 
-        `|  ${handleCellText('date', config.dateLength)}  ` + 
-        `|  ${handleCellText('comment', config.commentLength)}  ` +
-        `|  ${handleCellText('fileName', config.fileNameLength)}  \n`;
-    table += '-'.repeat(config.fullLength) + '\n';
+    let table = `  !  |  ${handleCellText('user', config.userLength)}  `
+        + `|  ${handleCellText('date', config.dateLength)}  `
+        + `|  ${handleCellText('comment', config.commentLength)}  `
+        + `|  ${handleCellText('fileName', config.fileNameLength)}  \n`;
+    table += `${'-'.repeat(config.fullLength)}\n`;
 
-    for (let comment of data) {
+    for (const comment of data) {
         table += formTableRow(comment, config);
     }
     if (data.length > 0) {
-        table += '-'.repeat(config.fullLength) + '\n';
+        table += `${'-'.repeat(config.fullLength)}\n`;
     }
 
     console.log(table);
 }
 
 module.exports = {
-    printTable
+    printTable,
 };
